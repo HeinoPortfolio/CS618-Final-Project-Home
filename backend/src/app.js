c158610394deda3e547b8753a33c352a5e7c9449
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-12 10:09:26 -0400

    chore: setup routes and tested some

--- backend/src/app.js
+++ backend/src/app.js
@@ -1,15 +1,16 @@
 // Express setup file =========================================================
 import express from 'express'
-//import { recipesRoutes } from './routes/recipes.js'
-//import { userRoutes } from './routes/users.js'
-//import bodyParser from 'body-parser'
+import { recipesRoutes } from './routes/recipes.js'
+import bodyParser from 'body-parser'
 //import cors from 'cors'
+//import { userRoutes } from './routes/users.js'
 
 const app = express()
-//app.use(bodyParser.json())
+app.use(bodyParser.json())
 //app.use(cors())
 
-//recipesRoutes(app)
+// Recipe routes ==============================================================
+recipesRoutes(app)
 
 // User route ========================
 //userRoutes(app)
