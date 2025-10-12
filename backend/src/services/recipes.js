04071a0f609a44e1bd58fdd887ec368de355cef9
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-11 23:22:00 -0400

    chore: setup the basic service layer functions

---
+++ backend/src/services/recipes.js
@@ -0,0 +1,58 @@
+import { Recipe } from '../db/models/recipe.js'
+
+// Create a recipe service function ===========================================
+export async function createRecipe({
+  title,
+  author,
+  ingredientList,
+  imageURL,
+}) {
+  const recipe = new Recipe({ title, author, ingredientList, imageURL })
+
+  return await recipe.save()
+} // End create recipe
+
+// List all the recipes =======================================================
+// It is a helper/common function==============================================
+async function listRecipes(
+  query = {},
+
+  { sortBy = 'createdAt', sortOrder = 'descending' } = {},
+) {
+  return await Recipe.find(query).sort({ [sortBy]: sortOrder })
+}
+// List all the recipes =======================================================
+export async function listAllRecipes(options) {
+  return await listRecipes({}, options)
+}
+// List recipes by an author ==================================================
+export async function listRecipesByAuthor(author, options) {
+  return await listRecipes({ author }, options)
+}
+// List all recipes by tags ===================================================
+export async function listRecipesByTag(tags, options) {
+  return await listRecipes({ tags }, options)
+}
+
+// Get the recipe by the recipe ID ============================================
+// Will use the Mongoose feature (findById)to find the recipe by the ID =======
+export async function getRecipeById(recipeId) {
+  return await Recipe.findById(recipeId)
+}
+
+// Update the recipe given a recipe ID ========================================
+export async function updateRecipe(
+  recipeId,
+  { title, author, ingredientList, imageURL, tags },
+) {
+  return await Recipe.findOneAndUpdate(
+    { _id: recipeId },
+    { $set: { title, author, ingredientList, imageURL, tags } },
+    { new: true },
+  )
+}
+
+// Delete a post given a recipe ID ============================================
+export async function deleteRecipe(userId, recipeId) {
+  return await Recipe.deleteOne({ _id: recipeId })
+}
