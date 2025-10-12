a553d2a4345a780b66a02f53d93ef707e33e127b
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-12 13:23:12 -0400

    chore: setup fetching blog posts

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -2,28 +2,19 @@ import { RecipeList } from './components/RecipeList.jsx'
 import { CreateRecipe } from './components/CreateRecipe.jsx'
 import { RecipeFilter } from './components/RecipeFilter.jsx'
 import { RecipeSorting } from './components/RecipeSorting.jsx'
-
-// Create some test recipes =============================================
-
-const recipes = [
-  {
-    title: 'This is a test recipe title',
-    ingredientList: 'Some ingredients go here. \nSome others go here.',
-    author: 'Matthew Heino',
-    imageURL: 'http://someUrl1.com',
-  },
-  {
-    title: 'This is another test recipe title',
-    ingredientList: 'Some other ingredients go here. \nMore go here.',
-    author: 'Claudia Heino',
-    imageURL: 'http://someUrl1.com',
-  },
-]
+import { useQuery } from '@tanstack/react-query'
+import { getRecipes } from './api/recipes.js'
 
 // Create the frontend of the application =====================================
 export function Blog() {
-  //return <RecipeList recipes={recipes} />
-  //return <CreateRecipe />
+  const recipesQuery = useQuery({
+    queryKey: ['recipes'],
+    queryFn: () => getRecipes(),
+  })
+
+  // Get the data from the query ==============================================
+  const recipes = recipesQuery.data ?? []
+
   return (
     <div style={{ padding: 8 }}>
       <CreateRecipe />
---
+++ src/api/recipes.js
@@ -0,0 +1,10 @@
+// An API file for recipes project
+
+// Function to get the recipes from the database server =========================
+export const getRecipes = async (queryParams) => {
+  const res = await fetch(
+    `${import.meta.env.VITE_BACKEND_URL}/recipes?` +
+      new URLSearchParams(queryParams),
+  )
+  return await res.json()
+}
