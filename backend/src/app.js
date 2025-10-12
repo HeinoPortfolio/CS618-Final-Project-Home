8471db247a42e26f89d343f5f6047087429888ea
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-12 10:17:27 -0400

    chore:  enabled cors

--- backend/src/app.js
+++ backend/src/app.js
@@ -2,22 +2,23 @@
 import express from 'express'
 import { recipesRoutes } from './routes/recipes.js'
 import bodyParser from 'body-parser'
-//import cors from 'cors'
+import cors from 'cors'
 //import { userRoutes } from './routes/users.js'
 
 const app = express()
 app.use(bodyParser.json())
-//app.use(cors())
+app.use(cors())
 
 // Recipe routes ==============================================================
 recipesRoutes(app)
 
 // User route ========================
 //userRoutes(app)
-
+/*
 app.get('/', (req, res) => {
   res.send('Hello from Express Live!  Using Nodemon')
 })
+*/
 
 // Export the app so it can be used in other files ====
 export { app }
