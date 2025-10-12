5428a85222bcf59bc8f856031acd4dd08fe854ab
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-12 15:19:18 -0400

    feat: implemented sorting and filtering for recipes

--- src/Blog.jsx
+++ src/Blog.jsx
@@ -5,11 +5,19 @@ import { RecipeSorting } from './components/RecipeSorting.jsx'
 import { useQuery } from '@tanstack/react-query'
 import { getRecipes } from './api/recipes.js'
 
+import { useState } from 'react'
+
 // Create the frontend of the application =====================================
 export function Blog() {
+  // Create the states ========================================================
+  const [author, setAuthor] = useState('')
+  const [sortBy, setSortBy] = useState('createdAt')
+  const [sortOrder, setSortOrder] = useState('descending')
+
+  // Create the recipes query =================================================
   const recipesQuery = useQuery({
-    queryKey: ['recipes'],
-    queryFn: () => getRecipes(),
+    queryKey: ['recipes', { author, sortBy, sortOrder }],
+    queryFn: () => getRecipes({ author, sortBy, sortOrder }),
   })
 
   // Get the data from the query ==============================================
@@ -21,9 +29,19 @@ export function Blog() {
       <br />
       <hr />
       <b>Filter by:</b>
-      <RecipeFilter field='author' />
+      <RecipeFilter
+        field='author'
+        value={author}
+        onChange={(value) => setAuthor(value)}
+      />
       <br />
-      <RecipeSorting fields={['createdAt', 'updatedAt']} />
+      <RecipeSorting
+        fields={['createdAt', 'updatedAt']}
+        value={sortBy}
+        onChange={(value) => setSortBy(value)}
+        orderValue={sortOrder}
+        onOrderChange={(orderValue) => setSortOrder(orderValue)}
+      />
       <hr />
       <RecipeList recipes={recipes} />
     </div>
--- src/components/RecipeSorting.jsx
+++ src/components/RecipeSorting.jsx
@@ -1,10 +1,22 @@
 import PropTypes from 'prop-types'
 
-export function RecipeSorting({ fields = [] }) {
+// Component for sorting the recipes based on options =========================
+export function RecipeSorting({
+  fields = [],
+  value,
+  onChange,
+  orderValue,
+  onOrderChange,
+}) {
   return (
     <div>
       <label htmlFor='sortBy'>Sort By:</label>
-      <select name='sortBy' id='sortBy'>
+      <select
+        name='sortBy'
+        id='sortBy'
+        value={value}
+        onChange={(e) => onChange(e.target.value)}
+      >
         {fields.map((field) => (
           <option key={field} value={field}>
             {field}
@@ -13,7 +25,12 @@ export function RecipeSorting({ fields = [] }) {
       </select>
       {'/'}
       <label htmlFor='sortOrder'>Sort Order:</label>
-      <select name='sortOrder' id='sortOrder'>
+      <select
+        name='sortOrder'
+        id='sortOrder'
+        value={orderValue}
+        onChange={(e) => onOrderChange(e.target.value)}
+      >
         <option value={'ascending'}>Ascending </option>
         <option value={'descending'}>Descending </option>
       </select>
@@ -22,4 +39,8 @@ export function RecipeSorting({ fields = [] }) {
 }
 RecipeSorting.propTypes = {
   fields: PropTypes.arrayOf(PropTypes.string).isRequired,
+  value: PropTypes.string.isRequired,
+  onChange: PropTypes.func.isRequired,
+  orderValue: PropTypes.string.isRequired,
+  onOrderChange: PropTypes.func.isRequired,
 }
