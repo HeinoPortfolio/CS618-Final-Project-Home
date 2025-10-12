c681db77e8250f05909eb6fab5c5737b3d3ec2f6
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-11 22:22:40 -0400

    chore: setup basic jest environment

--- backend/src/db/init.js
+++ backend/src/db/init.js
@@ -2,9 +2,9 @@ import mongoose from 'mongoose'
 
 // Initialize the databae =====================================================
 export function initDatabase() {
-  const DATABASE_URL = 'mongodb://localhost:27017/recipes-blog'
+  //const DATABASE_URL = 'mongodb://localhost:27017/recipes-blog'
 
-  //const DATABASE_URL = process.env.DATABASE_URL
+  const DATABASE_URL = process.env.DATABASE_URL
 
   mongoose.connection.on('open', () => {
     console.info('Successfully connected to the  database:', DATABASE_URL)
