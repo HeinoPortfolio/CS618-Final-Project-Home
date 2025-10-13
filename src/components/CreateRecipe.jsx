4e30cda466ed1c3642a25aab394b887c534d8a71
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-13 16:10:57 -0400

    feat: create recipe as a authenticated user

--- src/components/CreateRecipe.jsx
+++ src/components/CreateRecipe.jsx
@@ -2,20 +2,25 @@ import { useState } from 'react'
 import { useMutation, useQueryClient } from '@tanstack/react-query'
 import { createRecipe } from '../api/recipes.js'
 
+import { useAuth } from '../contexts/AuthContext.jsx'
+
 // Create a recipe component ==================================================
 export function CreateRecipe() {
   // Create the states=========================================================
   const [title, setTitle] = useState('')
-  const [author, setAuthor] = useState('')
+  //const [author, setAuthor] = useState('')
   const [ingredientList, setIngredientList] = useState('')
   const [imageURL, setImageURL] = useState('')
+
+  // Get the authentication context ===========================================
+  const [token] = useAuth()
 
   // Create the query client ==================================================
   const queryClient = useQueryClient()
 
   // Create the mutation for creating the new recipe ==========================
   const createRecipeMutation = useMutation({
-    mutationFn: () => createRecipe({ title, author, ingredientList, imageURL }),
+    mutationFn: () => createRecipe(token, { title, ingredientList, imageURL }),
     onSuccess: () => queryClient.invalidateQueries(['recipes']),
   })
 
@@ -24,6 +29,10 @@ export function CreateRecipe() {
     e.preventDefault()
     createRecipeMutation.mutate()
   }
+
+  // If there is no token ask to create an account
+  if (!token) return <div>Please login to create a new recipe.</div>
+
   return (
     <form onSubmit={handleSubmit}>
       <div>
@@ -39,19 +48,6 @@ export function CreateRecipe() {
         />
       </div>
       <br />
-      <div>
-        <label htmlFor='create-author'>
-          {' '}
-          <b>Author: </b>
-        </label>
-        <input
-          type='text'
-          name='create-author'
-          id='create-author'
-          value={author}
-          onChange={(e) => setAuthor(e.target.value)}
-        />
-      </div>
       <br />
       <div>
         <label htmlFor='create-imageURL'>
