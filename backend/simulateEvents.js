9131c3e16d8c49c2bfbd974634a71809ad70dfae
HeinoPorfolio <meheino@sbcglobal.net>
2025-10-30 20:23:55 -0400

    fix: changed the simulate events script

--- backend/simulateEvents.js
+++ backend/simulateEvents.js
@@ -12,9 +12,9 @@ dotenv.config()
 const simulationStart = Date.now() - 1000 * 60 * 60 * 24 * 30
 const simulationEnd = Date.now()
 
-const simulatedUsers = 3
+const simulatedUsers = 2
 const simulatedPosts = 3
-const simulatedViews = 5
+const simulatedViews = 10
 
 async function simulateEvents() {
   const connection = await initDatabase()
